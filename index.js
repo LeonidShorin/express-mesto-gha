14042dd54773e1939d21c79257f9283f461372f5
Leonid Shorin <stanvnmm@yandex.ru>
2022-04-02 10:43:56 +0500

    pr14

--- index.js
+++
@@ -1,8 +0,0 @@
-const { PORT = 3000 } = process.env;
-
-// eslint-disable-next-line no-undef
-const app = express();
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
